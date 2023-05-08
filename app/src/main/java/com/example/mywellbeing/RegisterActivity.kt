39ec7045d4cd9ced4e9c20072bd426aad5f08756
elsiegithae <elsiegithae4@gmail.com>
2023-05-08 10:48:28 +0300

    Added update functionality

--- app/src/main/java/com/example/mywellbeing/RegisterActivity.kt
+++ app/src/main/java/com/example/mywellbeing/RegisterActivity.kt
@@ -1,12 +1,43 @@
 package com.example.mywellbeing
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract.CommonDataKinds.Email
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.database.DatabaseReference
 
 class
 RegisterActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityRegisterBinding
+    private lateinit var database: DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnRegister.setOnClickListener{
+            val fullName = binding.fullName.text.toString()
+            val userName = binding.userName.text.tostring()
+            val email = binding.email.text.tostring()
+            val password = binding.password.text.tostring()
+
+            database = FirebaseDatabase.getInstance().getReference("Users")
+            val User = User(fullName, userName, email, password)
+            database.child(userName).setValue(User).addOnSuccessListener {
+
+                binding.fullName.text.clear()
+                binding.userName.text.clear()
+                binding.email.text.clear()
+                binding.password.text.clear()
+
+                Toast.makeText(this,"Successfully saved!", Toast.LENGTH_SHORT).show()
+            }.addOnFailureListener{
+                Toast.makeText(this,"Failed", Toast.LENGTH_SHORT).show()
+
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/mywellbeing/User.kt
@@ -0,0 +1,3 @@
+package com.example.mywellbeing
+
+data class User(val fullName : String? =null, val userName : String? =null, val email:String?=null, val password:String?=null)
